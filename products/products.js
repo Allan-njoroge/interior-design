0e98fb66eda7b32157828ef07e198d10ce35816c
Allan-njoroge <allandavis254@gmail.com>
2024-03-03 17:53:20 +0300

    Update

--- products/products.js
+++ products/products.js
@@ -16,3 +16,34 @@ menuIcon.addEventListener('click', () => {
         menuItems.classList.add("menuClosed");
     }
 });
+
+
+/*let closeBtn = document.querySelector("#close_btn");
+let buyBtns = Array.from(document.querySelectorAll(".buy_btn"));
+let formSection = document.querySelector(".form_section");
+let formSectionOpen = false;
+
+closeBtn.addEventListener('click', () => {
+    formSection.classList.remove("formSectionOpened");
+    formSection.classList.add("formSectionClosed");
+    formSectionOpen = false;
+    
+});
+
+buyBtns.forEach(buyBtn => buyBtn.addEventListener('click', () => {
+    formSection.classList.remove("formSectionClosed");
+    formSection.classList.add("formSectionOpened");
+    formSectionOpen = true;
+}));*/
+
+document.getElementByclass('buy_btn').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('formContainer').style.display = 'block';
+    document.body.style.overflow = 'hidden';
+});
+
+document.getElementById('close_btn').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('formContainer').style.display = 'none';
+    document.body.style.overflow = 'auto';
+});
