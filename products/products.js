1689e8172386886e7ccfa19e7aec18d7abb3bcfe
Allan-njoroge <allandavis254@gmail.com>
2024-03-09 14:51:32 +0300

    Updated Form

--- products/products.js
+++ products/products.js
@@ -17,22 +17,57 @@ menuIcon.addEventListener('click', () => {
     }
 });
 
-
+//collection of elements and storing them in variables from the HTML sheet
 let buyBtns = Array.from(document.querySelectorAll('.buy_btn'));
 let overlay = document.querySelector(".overlay");
 let formContainer = document.querySelector(".form_container");
 let closeBtn = document.querySelector("#closebtn");
 let body = document.querySelector("body");
 
-buyBtns.forEach(buyBtn => buyBtn.addEventListener('click', () => {
+let finalProduct = document.querySelector(".final_product");
+let finalPrice = document.querySelector(".final_price");
+let orderBtn = document.querySelector(".order_btn");
+
+let prices = Array.from(document.querySelectorAll("#price"));
+let productNames = Array.from(document.querySelectorAll(".product_name"));
+
+let selectedPrice;//this variable stores the value of the specific product corresponding to button clicked
+
+buyBtns.forEach((buyBtn, i) => buyBtn.addEventListener('click', () => {
+    //style to display the shipping details form
     overlay.style.display = "block";
     formContainer.classList.add("formOpen");
     formContainer.classList.remove("formClosed");
     body.classList.add("no-scroll"); // Add class to disable scrolling
-}))
+    
+    //getting the product name for each specific product
+    let productName = productNames[i].innerText;
+
+    //variables for the final product name on the form
+    let finalProduct = document.querySelector(".final_product");
+
+    //getting the price for each specific product
+    let price = prices[i].innerText;
+    selectedPrice = parseInt(price);
+
+    finalProduct.textContent = productName;
+}));
+
+//variable for the quantity of the products
+let quantity = document.querySelector("#quantity");
+quantity.addEventListener('input', (prices, i) => {
+    let quantityInput = parseInt(quantity.value);
+
+    //calculating the price by the quantity
+    let total = selectedPrice * quantityInput;
+
+    finalPrice.textContent = ` Ksh ${total}`;
+});
+
+//this function closes the form
 closeBtn.addEventListener('click', () => {
     overlay.style.display = "none";
     formContainer.classList.add("formClosed");
     formContainer.classList.remove("formOpen");
     body.classList.remove("no-scroll"); // Remove class to enable scrolling
-})
+});
