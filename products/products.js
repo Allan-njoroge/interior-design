eabd20abfbf9221fdad4107d0b2ddd6a1c0c04d6
Allan-njoroge <allandavis254@gmail.com>
2024-03-09 13:09:05 +0300

    Update

--- products/products.js
+++ products/products.js
@@ -22,14 +22,17 @@ let buyBtns = Array.from(document.querySelectorAll('.buy_btn'));
 let overlay = document.querySelector(".overlay");
 let formContainer = document.querySelector(".form_container");
 let closeBtn = document.querySelector("#closebtn");
+let body = document.querySelector("body");
 
 buyBtns.forEach(buyBtn => buyBtn.addEventListener('click', () => {
     overlay.style.display = "block";
     formContainer.classList.add("formOpen");
     formContainer.classList.remove("formClosed");
+    body.classList.add("no-scroll"); // Add class to disable scrolling
 }))
 closeBtn.addEventListener('click', () => {
     overlay.style.display = "none";
     formContainer.classList.add("formClosed");
     formContainer.classList.remove("formOpen");
+    body.classList.remove("no-scroll"); // Remove class to enable scrolling
 })
